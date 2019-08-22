a643fadd1255e4a40597d6d118aac09e860f70bd
Laurent <rg@zef.com>
2019-08-22 11:35:57 +0200

    Timbre & Test_1

--- laposteProject/src/test/java/Page/Test_1.java
+++ laposteProject/src/test/java/Page/Test_1.java
@@ -15,16 +15,16 @@ public class Test_1 {
 	public void test0() {
 		timbre.setSpinner1(2);
 		timbre.setSpinner2(0);
-		//String total = timbre.getTotalStr(); A revoir...
-		//assertEquals("18", total);
+		int total = timbre.getTotal();
+		assertEquals("18", total);
 	}
 	
 	@Test
 	public void test1() {
 		timbre.setSpinner1(0);
 		timbre.setSpinner2(5);
-		//String total=timbre.getTotalStr(); A revoir...
-		//assertEquals("5", total);
+		int total=timbre.getTotal();
+		assertEquals("5", total);
 		
 	}
 	
@@ -32,8 +32,8 @@ public class Test_1 {
 	public void test2() {
 		timbre.setSpinner1(3);
 		timbre.setSpinner2(3);
-		//String total=timbre.getTotalStr(); A revoir...
-		//assertEquals("30", total);
+		int total=timbre.getTotal();
+		assertEquals("30", total);
 	}
 
 
