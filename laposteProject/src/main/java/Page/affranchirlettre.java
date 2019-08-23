3fdbdcd77bb9cd7e03fa0c344619572f309deaa4
Laurent <rg@zef.com>
2019-08-23 15:13:02 +0200

    Correction

--- laposteProject/src/main/java/Page/affranchirlettre.java
+++ laposteProject/src/main/java/Page/affranchirlettre.java
@@ -154,7 +154,7 @@ public class affranchirlettre {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				frame.setVisible(false);
-				Accueil.getPanier().resetPanierValue();
+				//Accueil.getPanier().resetPanierValue();
 				Accueil.main(null);
 		
 		}
